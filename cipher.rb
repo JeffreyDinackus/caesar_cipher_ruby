3a0add3f5fe5fd8d008d72e4c8c1a12f82b8c4ef
JeffreyDinackus <jeffreydinackus@proton.me>
2023-12-14 16:42:38 -0500

    uppercase support added

--- cipher.rb
+++ cipher.rb
@@ -8,34 +8,52 @@ puts "enter a string to be encrypted"
 input = gets
 
 lowerhash = Hash.new
-
+upperhash = {}
 
 (0..25).each do |i|
   lowerhash[lowercase[i]] = i
-
+  upperhash[uppercase[i]] = i
 end
 
-puts lowerhash
 puts "enter a shift to the right"
 shift = gets.to_i
 
 flipped_hash_lower = lowerhash.invert
+flipped_hash_upper = upperhash.invert
 
 
 final_str = ""
+
+def handle_working(working, shift)
+    final_index = working + shift
+    if final_index > 25
+      final_index -= 25 + 1
+
+    end
+    puts final_index
+    final_index
+end
 
 input.each_char do |c|
   if c.include? "\n"
 
     break
   end
-  working = lowerhash[c].to_i
-  final_index = working + shift
-  if final_index > 25
-    final_index - 25
+  if lowercase.include?(c)
+    working = lowerhash[c].to_i
+    final_index = handle_working(working, shift)
 
+    final_str += flipped_hash_lower[final_index]
   end
-  final_str += flipped_hash_lower[final_index]
+  if uppercase.include?(c)
+    working = upperhash[c].to_i
+    final_index = handle_working(working, shift)
+    puts flipped_hash_upper[final_index-1]
+    final_str += flipped_hash_upper[final_index]
+  end
+
+
+
 end
 
 print(final_str)
